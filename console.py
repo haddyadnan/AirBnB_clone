5ff6918f5ddfdf5ccca94111848b18fa3ff72aa8
Ezekiel Bassey <Bazzdude007@gmail.com>
2022-08-02 04:33:13 +0100

    Add BaseModel object handlers with no implementation

--- console.py
+++ console.py
@@ -23,6 +23,33 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    def do_create(self, *args):
+        """Create new instance of BaseModel and prints the id"""
+        print("done create")
+
+    def do_show(self, *args):
+        """Prints the string representation of an instance based
+        the class name and id
+        """
+        print("done show")
+
+    def do_destroy(self, *args):
+        """Deletes an instance based on the class name and id
+        """
+        print("done destroy")
+
+    def all(self, *args):
+        """Prints all string representation of all instances based or not
+        on the class name
+        """
+        print("done all")
+
+    def update(self, *args):
+        """Updates an instance based on the class and id by adding or
+        updating attribute
+        """
+        print("done update")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()

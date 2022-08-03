04af6da393c9b06fd3f41b565d30f629df448e90
Ezekiel Bassey <Bazzdude007@gmail.com>
2022-08-03 14:42:19 +0100

    Update console.py, works in non-interactive mode, completed do_create and do_show function

--- console.py
+++ console.py
@@ -7,10 +7,12 @@ This is the console module
 import cmd
 import sys
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """The HBNBCommand class"""
+    all_objs = {}
     prompt = "(HBNB) "
 
     def emptyline(self):
@@ -41,28 +43,21 @@ class HBNBCommand(cmd.Cmd):
         """Prints the string representation of an instance based \
         the class name and id"""
         arguments = args(line)
-        if len(arguments) > 0:
-            if arguments[0] == "BaseModel":
-                if len(arguments) > 1:
-                    if arguments[1] in HBNBCommand.__JSON_file:
-                        print("done show")
-                    else:
-                        print("** no instance found **")
-                else:
-                    print("** instance id missing **")
-            else:
-                print("** class doesn't exist **")
-        else:
-            print("** class name missing **")
+        if input_check(arguments):
+            obj = HBNBCommand.all_objs[\
+                    "{}.{}".format(arguments[0], arguments[1])]
+            print(obj)
 
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id"""
-        print("done destroy")
+        arguments = args(line)
+        if input_check(arguments):
+            print("done destroy")
 
-    def all(self, line):
+    def do_all(self, line):
         """Prints all string representation of all instances based or not \
         on the class name"""
-        print("done all")
+        print(HBNBCommand.all_objs)
 
     def update(self, line):
         """Updates an instance based on the class and id by adding or \
@@ -74,6 +69,24 @@ def args(arg):
     """Convert a line string to an argument tuple"""
     return tuple(arg.split())
 
+def input_check(a_tuple) -> bool:
+    if len(a_tuple) > 0:
+        if a_tuple[0] == "BaseModel":
+            if len(a_tuple) > 1:
+                if "{}.{}".format(a_tuple[0], a_tuple[1])\
+                in HBNBCommand.all_objs.keys():
+                    return True
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+    else:
+        print("** class name missing **")
+
 
 if __name__ == "__main__":
+    storage.reload()
+    HBNBCommand.all_objs = storage.all()
     HBNBCommand().cmdloop()

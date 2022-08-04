000f4742b334528ee3e380d42e39c432b9a983d7
haddyadnan <haddyadnan@gmail.com>
2022-08-04 23:57:11 +0100

    Added more classes

---
+++ models/amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module contains the amenity class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+
+    """
+    Implementation of the amenity class
+    Attr:
+        name
+    """
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module contains the city class
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+
+    """
+    Implementation of the City class
+    Attr:
+        state_id
+        name
+    """
+
+    state_id = ""
+    name = ""

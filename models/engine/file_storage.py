c9814c68129c2b5dc591c2ec2c5e02ee6e53ca81
haddyadnan <haddyadnan@gmail.com>
2022-08-04 02:23:37 +0100

    fixed object bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,9 +4,10 @@
 This module contains the class for persistent file storage
 """
 
-from cgi import FieldStorage
 from genericpath import exists
 import json
+from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
@@ -34,7 +35,14 @@ class FileStorage:
         sets in __objects the obj with key <obj class name>.id
         """
         obj_key = obj.__class__.__name__ + "." + obj.id
+
         FileStorage.__objects[obj_key] = obj
+        ##############DEBUGGING#############
+        # print()
+        # print("This is the object")
+        # print(obj)
+        # print(obj, "------------------object")
+        # print(obj.__dict__, "----------------dict")
 
     def save(self) -> None:
 
@@ -43,8 +51,9 @@ class FileStorage:
         """
         json_obj = {}
         for key, value in FileStorage.__objects.items():
-            json_obj[key] = str(value)
-        with open(FileStorage.__file_path, "w") as f:
+            # print(f"The value is: {value}")
+            json_obj[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             json.dump(json_obj, f)
 
     def reload(self) -> None:
@@ -52,8 +61,19 @@ class FileStorage:
         """
         deserializes the json file to __objects if __file_path exists
         """
+
         if exists(FileStorage.__file_path):
+            FileStorage.__objects = {}
             with open(FileStorage.__file_path, "r") as f:
                 load_dict = json.load(f)
-            for k, v in load_dict.items():
-                FileStorage.__objects[k] = v
+                # print()
+                # print()
+                # print("------- RELOADING HERE ----------")
+                # print(load_dict.items())
+                # print("-------------------DONE--------------")
+                # print()
+                for k, v in load_dict.items():
+                    cls_name = k.split(".")[0]
+                    FileStorage.__objects[k] = eval(cls_name)(**v)
+                # print(FileStorage.__objects)
+                # print("________FINALLY DONE_____________")
